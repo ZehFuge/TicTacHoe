49515f4e5d3c5521b92414eff0cdba21f714bb08
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-10-14 07:43:15 +0200

    Update window_settings.py

--- R-Type_Star_Fox/window_settings.py
+++ R-Type_Star_Fox/window_settings.py
@@ -3,13 +3,16 @@ import pygame
 pygame.init()
 
 # set window information
-windowWidth = 800
-windowHeigth = 600
+windowWidth = 1200
+windowHeigth = 1000
 window = pygame.display.set_mode((windowWidth, windowHeigth))
+pygame.display.set_caption("Star Fox Tournament")
+pygame.display.toggle_fullscreen()
+background = pygame.image.load("Images/background_frame_1200x1000.png")
 
 # set time information
 clock = pygame.time.Clock()
-FPS = 45
+FPS = 60
 
 # pre defined colors
 BLACK = (0, 0, 0)
@@ -17,3 +20,4 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+CONV_PINK = (255, 174, 201)
